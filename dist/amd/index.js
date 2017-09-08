3ab6064c38875e3454922ffe63cf3b7174458d73
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-08 17:24:04 +0200

    :fire: remove dist-folder

--- dist/amd/index.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "./Processable/Table/Table", "./Processable/CrudTable/CrudTable", "./Processable/ProcessableContainer"], function (require, exports, Table_1, CrudTable_1, ProcessableContainer_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.ProcessableTable = Table_1.default;
-    exports.ProcessableCrudTable = CrudTable_1.default;
-    exports.ProcessableContainer = ProcessableContainer_1.default;
-});
-
-//# sourceMappingURL=index.js.map
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProcessableTable } from './Processable/Table/Table';
-export { default as ProcessableCrudTable } from './Processable/CrudTable/CrudTable';
-export { default as ProcessableContainer } from './Processable/ProcessableContainer';
-export * from './interfaces';
